40fae4310c1891ba600bb5002c65b7aadd54e61a
PressFnuy <100027686+DenisShapovalov1@users.noreply.github.com>
2022-03-03 20:27:26 +0300

    Add files via upload

---
+++ calculate.py
@@ -0,0 +1,16 @@
+from ffffff import  *
+import sys
+from PyQt5 import QtWidgets
+
+app = QtWidgets.QApplication(sys.argv)
+Form = QtWidgets.QMainWindow()
+ui = Ui_MainWindow()
+ui.setupUi(Form)
+Form.show()
+sys.exit(app.exec())
+
+                                             
+
+            
+
+
